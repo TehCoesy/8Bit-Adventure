8a5300f26806d599d5d9e09b50a191edb925296e
nghianv1 <69795795+nghianv1@users.noreply.github.com>
2020-08-23 21:12:59 +0700

    Add files via upload
    +Updated some basic methods

--- Source/MainMenu.h
+++ Source/MainMenu.h
@@ -7,11 +7,20 @@
 #include "Singleton.h"
 #include "MyObject.h"
 
-class MainMenu {
+class MainMenu : public Singleton<MainMenu> {
 private:
+	//sf::Sprite menuSprite;
+	sf::Texture* menuTexture;
+	sf::Font font, titleFont;
+	const int NUM_BUTTONS = 3;
+	sf::Text title;
+	std::vector<sf::Text> buttons;
 public:
 	MainMenu();
 	~MainMenu();
-
+	void Update(const float dt);
+	void Render(sf::RenderWindow *window);
+	void HandleInput();
 	void LoadFromFile(std::string strFilePath);
+	sf::Sprite* menuSprite;
 };
