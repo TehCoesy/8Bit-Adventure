ae03a26be20a65b7a3f12b3366b07a5e928eb9ad
Đức Hoàng <duc.ht2699@gmail.com>
2020-08-31 05:34:51 +0700

    - Code StateManger and add State class

--- Source/StateManager.cpp
+++ Source/StateManager.cpp
@@ -4,9 +4,60 @@
 #include "StateManager.h"
 
 StateManager::StateManager() {
-
 }
 
 StateManager::~StateManager() {
+}
 
+void StateManager::AddState(StateRef newState, bool isReplacing) {
+	this->m_isAdding = true;
+	this->m_isReplacing = isReplacing;
+
+	this->m_srNewState = std::move(newState);
+}
+
+void StateManager::AddState(StateRef newState) {
+	this->m_isAdding = true;
+	this->m_isReplacing = true;
+
+	this->m_srNewState = std::move(newState);
+}
+
+void StateManager::RemoveState() {
+	this->m_isRemoving = true;
+}
+
+void StateManager::ProcessStateChanges()
+{
+	if (this->m_isRemoving && !this->m_aState.empty())
+	{
+		this->m_aState.pop();
+		if (!this->m_aState.empty())
+		{
+			this->m_aState.top()->Resume();
+		}
+		this->m_isRemoving = false;
+	}
+
+	if (this->m_isAdding)
+	{
+		if (!this->m_aState.empty())
+		{
+			if (this->m_isReplacing)
+			{
+				this->m_aState.pop();
+			}
+			else
+			{
+				this->m_aState.top()->Pause();
+			}
+		}
+		this->m_aState.push(std::move(this->m_srNewState));
+		this->m_aState.top()->Init();
+		this->m_isAdding = false;
+	}
+}
+StateRef& StateManager::GetActiveState()
+{
+	return this->m_aState.top();
 }
--- Source/StateManager.h
+++ Source/StateManager.h
@@ -1,14 +1,31 @@
 #pragma once
-
+#include <memory>
+#include <stack>
 // Precompiled Headers
 #include "stdafx.h"
 
 // Local
 #include "Singleton.h"
+#include "State.h"
 
+typedef std::unique_ptr<State> StateRef;
 class StateManager : public Singleton<StateManager> {
 private:
+	std::stack<StateRef> m_aState;
+	StateRef m_srNewState;
+
+	bool m_isRemoving;
+	bool m_isAdding;
+	bool m_isReplacing;
 public:
 	StateManager();
 	~StateManager();
+
+	void AddState(StateRef newState, bool isReplacing = true);
+	void AddState(StateRef newState);
+	void RemoveState();
+
+	void ProcessStateChanges();
+
+	StateRef& GetActiveState();
 };
